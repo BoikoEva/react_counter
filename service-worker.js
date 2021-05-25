472acf2470d27dfc5c7c628d3564b7c9e42389f5
Eva <eva@MacBook-Air-Eva.local>
2021-05-25 14:17:43 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.35369277eabbfb9f41d8e1c2d600fc7c.js"
+  "./precache-manifest.264095f367234be5622bcbbb5c18f5e7.js"
 );
 
 self.addEventListener('message', (event) => {
